e7df38b069abe959e1aef3e5b8b12d3742f2c51a
shahadh7 <shahah.hamza@gmail.com>
2025-12-30 19:29:51 +0530

    fix

--- apps/init-db.sql
+++ apps/init-db.sql
@@ -1,6 +1,12 @@
+-- Create the genuinegrads database (alias for shared)
+-- Note: genuinegrads_shared is created by default via POSTGRES_DB env var
+-- This creates an alias database for backward compatibility
+CREATE DATABASE genuinegrads;
+
 -- Create the university template database
 CREATE DATABASE genuinegrads_uni_template;
 
 -- Grant privileges
+GRANT ALL PRIVILEGES ON DATABASE genuinegrads TO genuinegrads;
 GRANT ALL PRIVILEGES ON DATABASE genuinegrads_shared TO genuinegrads;
 GRANT ALL PRIVILEGES ON DATABASE genuinegrads_uni_template TO genuinegrads;
